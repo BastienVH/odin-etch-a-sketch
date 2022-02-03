3e41b296277a2e9bdd2bde2129dbea64d4d40846
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-02-03 14:25:33 +0100

    added function to empty the existing grid

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
 // Function that draws a square of divs
 
 const container = document.querySelector("div.container");
-drawGrid();
+drawGrid(16);
 
-function drawGrid() {
+function drawGrid(num) {
+  eraseGrid();
   // set correct amount of rows and columns
-  container.style.cssText += 'grid-template-columns:repeat(4, 1fr);grid-template-rows:repeat(4, 1fr)';
-  for (let i = 0; i < 16; i++) {
+  container.style.cssText += `grid-template-columns:repeat(${num}, 1fr);grid-template-rows:repeat(${num}, 1fr)`;
+  for (let i = 0; i < num*num; i++) {
     // create div with number
     box = document.createElement('div.box');
-    box.textContent = `This is box ${i}`;
+    box.style.cssText = "border:1px solid black"
     container.appendChild(box);
+  }
+}
+
+function eraseGrid() {
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
   }
 }
