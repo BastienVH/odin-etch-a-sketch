904ff0f908eb5edaa0a4896f72ead63df6d7c3df
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-02-04 13:55:05 +0100

    fixed place to add event listener to all boxes

--- scripts.js
+++ scripts.js
@@ -8,17 +8,15 @@ btn.addEventListener("click", promptForSize);
 
 function drawGrid(num) {
   eraseGrid();
-  // set correct amount of rows and columns
   container.style.cssText += `grid-template-columns:repeat(${num}, 1fr);grid-template-rows:repeat(${num}, 1fr)`;
   for (let i = 0; i < num*num; i++) {
-    // create div with number
     box = document.createElement("div");
     box.classList.add("box");
     box.style.cssText = "border:1px solid black"
     container.appendChild(box);
-    const boxes = document.querySelectorAll("div.box");
-    boxes.forEach(box => box.addEventListener("mouseover", applyColor));
   }
+  const boxes = document.querySelectorAll("div.box");
+  boxes.forEach(box => box.addEventListener("mouseover", applyColor));
 }
 
 function eraseGrid() {
